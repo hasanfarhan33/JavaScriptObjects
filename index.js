09cbf2ecbb3500adac695df42ca6561e1757ac66
Farhan Hasan <hasanfarhan33@gmail.com>
2021-05-03 13:51:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let person = {
+    name: "Farhan",
+    age: 22
+
+};
+
+console.log(person);
+//Dot notation
+console.log(person.age);
+console.log(person.name);
+
+console.log("");
+
+//Bracket Notation 
+console.log(person['name']);
+console.log(person['age']);
